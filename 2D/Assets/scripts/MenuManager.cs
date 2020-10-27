870f9a841e6925a6ed81936d8082235e145206b6
Kuo0418 <71679871+Kuo0418@users.noreply.github.com>
2020-10-27 15:59:10 +0800

    新增選單

---
+++ 2D/Assets/scripts/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // 註解:說明
+    //按鈕如何與程式溝通
+    //需要:方式(函式、功能)-一堆有功能的程式，演算法
+    //語法:
+    //類型 方式名稱 () {}
+    // void 無類型
+    void StartGame()
+    {
+
+    }
+
+    void QuitGame()
+    {
+
+    }
+}
